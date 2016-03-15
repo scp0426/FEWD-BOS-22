7f8e3f6fbef3a207819b715e2ea274b8f3dc09b6
Michael Finneran <mrfinneran@gmail.com>
2016-03-15 17:23:17 -0400

    day 11

--- Week_05_JS_and_JQuery/09_Conditions_and_DOM/starter_code/traffic_light/js/index.js
+++ Week_05_JS_and_JQuery/09_Conditions_and_DOM/starter_code/traffic_light/js/index.js
@@ -1,25 +1,45 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+function clearLight(){
 
-$(document).ready(function() {
+  $(".bulb").css( "background-color", "black");
 
-	function turnOffLights(){
-		$(".bulb").css("background-color", "black");
+
+  // document.getElementById("slowLight").style.backgroundColor = "black";
+  // document.getElementById("goLight").style.backgroundColor = "black";
+  // document.getElementById("stopLight").style.backgroundColor = "black";
+}
+
+// turn on stop function 
+function turnOnStopLight(){
+	if (document.getElementById("stopLight").style.backgroundColor === "red"){
+		clearLight();
+	} else {
+		clearLight();
+  	document.getElementById("stopLight").style.backgroundColor = "red";
 	}
 
-	$("#stopButton").click(function(){
-		turnOffLights();
-		$("#stopLight").css("background-color", "red");
-		// $("#slowLight").css("background-color", "black");
-		// $("#goLight").css("background-color", "black");
-	});
+}
 
-	$("#slowButton").click(function(){
-		turnOffLights();
-		$("#slowLight").css("background-color", "yellow");
-	});
+// turn on slow function
+function turnOnSlowLight(){
+    clearLight();
+  document.getElementById("slowLight").style.backgroundColor = "yellow";
+}
 
-	$("#goButton").click(function(){
-		turnOffLights();
-		$("#goLight").css("background-color", "green");
-	});
-});
+// turn on go function
+function turnOnGoLight(){
+    clearLight();
+  document.getElementById("goLight").style.backgroundColor = "green";
+}
+
+// set stop light click event
+document.getElementById("stopButton").onclick = turnOnStopLight;
+
+// set slow light click event
+document.getElementById("slowButton").onclick = turnOnSlowLight;
+
+// set go light click event
+document.getElementById("goButton").onclick = turnOnGoLight;
+
+
+
+
